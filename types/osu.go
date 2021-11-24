49c60d22daa980435668621e4eeb052917f4791d
l3lackShark <lea2616@gmail.com>
2021-11-24 19:38:44 +0300

    types: export all structs

--- types/osu.go
+++ types/osu.go
@@ -29,14 +29,14 @@ type BeatmapInfo struct {
 	HpDrain                   float32
 	OverallDifficulty         float32
 	SliderVelocity            float64 //double
-	StarRatingOsu             []starRating
-	StarRatingTaiko           []starRating
-	StarRatingCtb             []starRating
-	StarRatingMania           []starRating
+	StarRatingOsu             []StarRating
+	StarRatingTaiko           []StarRating
+	StarRatingCtb             []StarRating
+	StarRatingMania           []StarRating
 	DrainTime                 int32
 	TotalTime                 int32
 	PreviewTime               int32
-	TimingPoints              []timingPoint
+	TimingPoints              []TimingPoint
 	BeatmapID                 int32
 	BeatmapSetID              int32
 	ThreadID                  int32
@@ -65,14 +65,14 @@ type BeatmapInfo struct {
 	ManiaScrollSpeed          uint8
 }
 
-type starRating struct {
+type StarRating struct {
 	Fixed0x08  uint8
 	BitMods    int32
 	Fixed0x0D  uint8
 	StarRating float64 //double
 }
 
-type timingPoint struct {
+type TimingPoint struct {
 	MsPerBeat            float64 //double
 	SongOffset           float64 //double
 	InheritedTimingPoint bool
